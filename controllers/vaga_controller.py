85c7c624296d693cdf8d154ec0c13be421991fa9
Renato Farias <santosrenato351@gmail.com>
2023-04-30 21:28:30 -0300

    otimizando o código

--- controllers/vaga_controller.py
+++ controllers/vaga_controller.py
@@ -1,32 +1,30 @@
 import json
 
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, request, jsonify
 
 from database.database import db
 from entities.vaga import Vaga
+from services.vaga_service import create_vaga, listar_vagas
 
 vagas_bp = Blueprint('vagas', __name__, url_prefix='/vagas')
 
 
 # Lista todas as vagas
 @vagas_bp.route('', methods=['GET'])
-def listar_vagas():
-    vagas = Vaga.query.all()
-    vagas_dict = [vaga.to_dict() for vaga in vagas]
-    vagas_json = json.dumps(vagas_dict, separators=(',', ':'))
+def find_all():
+    vagas_json = json.dumps(listar_vagas(), separators=(',', ':'))
     response = Response(vagas_json, status=200, mimetype='application/json')
     return response
 
 
-# Cria uma nova vaga
-@vagas_bp.route('', methods=['POST'])
-def criar_vaga():
-    job_id = request.json['job_id']
-    descricao_vaga = request.json['descricao_vaga']
-    nivel_vaga = request.json['nivel_vaga']
-    vaga = Vaga(job_id=job_id, descricao_vaga=descricao_vaga, nivel_vaga=nivel_vaga)
-    db.session.add(vaga)
-    db.session.commit()
-    vaga_json = json.dumps(vaga.to_dict(), separators=(',', ':'))
-    response = Response(vaga_json, status=201, mimetype='application/json')
-    return response
+@vagas_bp.route('/create', methods=['POST'])
+def create():
+    try:
+        job_id = request.json['job_id']
+        descricao_vaga = request.json['descricao_vaga']
+        nivel_vaga = request.json['nivel_vaga']
+        create_vaga(job_id, descricao_vaga, nivel_vaga)
+        return jsonify({'mensagem': 'Vaga criada com sucesso!'}), 200
+    except Exception as e:
+        error_msg = {'mensagem': 'Erro ao criar vaga: {}'.format(str(e))}
+        return jsonify(error_msg), 500
