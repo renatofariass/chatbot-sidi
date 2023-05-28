10ca21482c96575b9a8f96cbcc201bc154b7c9a2
Renato Farias <santosrenato351@gmail.com>
2023-05-28 13:57:53 -0300

    adicionando margem de erro nos comandos

--- servico_chatbot/chatbot/chatbot.py
+++ servico_chatbot/chatbot/chatbot.py
@@ -2,36 +2,36 @@ from nltk.chat import Chat
 from nltk.chat.util import reflections
 
 padroes = [
-    (r"!menu",
+    (r"!menu|!!menu",
      ["Comandos que você pode usar: \n !saber - Saber mais sobre o Chatbot SiDi \n !sidi - Redes sociais da SiDi "
       "\n !perguntas - Prosseguir para as perguntas \n !ajuda - Como navegar pelo Chatbot SiDi \n "]),
 
-    (r"!saber",
+    (r"!saber|!!saber",
      ["!serve - Para que o Chatbot SiDi serve? \n !funciona - Como funciona o ChatBot SiDi?"]),
 
-    (r"!ajuda",
+    (r"!ajuda|!!ajuda",
      ["Eu funciono através de comandos começados com '!'. Então para todos os comandos que quiser que eu execute coloque o '!' na frente da palavra."]),
     
-    (r"!sidi",
+    (r"!sidi|!!sidi",
      ["Siga a gente nas redes sociais para ficar por dentro das vagas. 🥰 \n "
       "LinkedIn: https://www.linkedin.com/company/segueosidi \n "
       "Instagram: https://www.instagram.com/segueosidi \n "
       "Facebook: https://www.facebook.com/segueosidi \n "
       "Linktree: https://linktr.ee/segueosidi"]),
 
-    (r"!perguntas", ["Ok, você quer ir para as perguntas. Por favor, informe o seu código de vaga. Ex. 123"]),
+    (r"!perguntas|!pergunta|!!pergunta|!!perguntas", ["Ok, você quer ir para as perguntas. Por favor, informe o seu código de vaga. Ex. 123"]),
 
-    (r"!serve", [
+    (r"!serve|!!serve", [
         "Eu ajudo no recrutamento de candidatos, avaliando se os candidatos estão aptos para as "
         "vagas ou não, com base nas perguntas que meus criadores disponibilizaram para as vagas."]),
 
-    (r"!funciona", [
+    (r"!funciona|!!funciona", [
         "Eu faço perguntas aos candidatos com base na vaga relacionada e se o candidato passar pela triagem das "
         "perguntas, eu salvo a inscrição dele no meu banco de dados."]),
 
     (r"(.*)seu nome(.*)|(.*)teu nome(.*)", ["Meu nome é ChatBot Sidi"]),
 
-    (r"(.*)(candidatar|inscrição|vaga)(.*)", ["Por favor, informe o seu código de vaga para prosseguir."]),
+    (r"(.*)(candidatar|inscrição|vaga)(.*)", ["Por favor, informe o seu código de vaga para prosseguir. Ex. 123"]),
 
     (r"(!sair)", ["Tchau, tchau! 👋😃"])
 ]
